431d6e1259daaab642c593fa8a3f00a1d4e53ba5
Olin Soffer <olinsoffer@outlook.com>
2017-08-23 09:54:38 +0300

    test

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-var coffeeShop = {
+let coffeeShop = {
   beans: 40,
+  money: 100,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {
+      price: 10,
+      beanRequirement: 2
+    },
+    americano: {
+      price: 5,
+      beanRequirement: 1
+    },
+    doubleShot: {
+      price: 15,
+      beanRequirement: 4
+    },
+    frenchPress: {
+      price: 12,
+      beanRequirement: 3
+    }
   },
 
   makeDrink: function (drinkType) {
-    // TODO: Finish this method
+    if(this.drinkRequirements[drinkType] && this.beans > this.drinkRequirements[drinkType].beanRequirement) {
+      this.beans -= this.drinkRequirements[drinkType].beanRequirement;
+      return true;  
+    }
+    else if (this.drinkRequirements[drinkType]) {
+      alert("Sorry we are out of beans");
+      return false;
+    }
+    else {
+      alert(`Sorry we dont make ${drinkType}`);
+      return false;
+    }
+  },
+  
+  buySupplies: function(price) {
+    this.beans-=price;
+  },
+
+  buyDrink: function(drink) {
+    if(this.makeDrink(drink) && this.drinkRequirements[drink]) {
+      this.money-=this.drinkRequirements[drink].price;
+    }
   }
 }
 
-coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
-coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress");
+coffeeShop.buyDrink("latte");
+coffeeShop.buyDrink("americano");
+coffeeShop.buyDrink("filtered");
+coffeeShop.buyDrink("doubleShot");
+coffeeShop.buyDrink("frenchPress");
